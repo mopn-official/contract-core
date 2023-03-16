90edb6cb70bb86873a4379394749c28d6df2e98a
cyanface <cyanface@outlook.com>
2023-03-16 17:24:22 +0800

    add fakenft for test

---
+++ contracts/TESTNFT1.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract TESTNFT1 is ERC721, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("MOPNTest1", "MOPNTest1") {}
+
+    function safeMint(address to, uint256 amount) public {
+        for (uint256 i = 0; i < amount; i++) {
+            _safeMint(to, _tokenIdCounter.current());
+            _tokenIdCounter.increment();
+        }
+    }
+
+    function nextTokenId1() public view returns (uint256) {
+        return _tokenIdCounter.current();
+    }
+
+    string public baseURI;
+
+    function setBaseURI(string memory baseURI_) public onlyOwner {
+        baseURI = baseURI_;
+    }
+
+    function _baseURI() internal view override returns (string memory) {
+        return baseURI;
+    }
+}
