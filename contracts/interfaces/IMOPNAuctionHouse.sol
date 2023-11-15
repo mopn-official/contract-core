9f0f03b571251d229b20b3493fb3ca724ad0848d
cyanface <cyanface@outlook.com>
2023-11-15 23:53:34 +0800

    remove trush files

--- contracts/interfaces/IMOPNAuctionHouse.sol
+++ contracts/interfaces/IMOPNAuctionHouse.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "@openzeppelin/contracts/interfaces/IERC1155.sol";
-
-interface IMOPNAuctionHouse is IERC1155 {
+interface IMOPNAuctionHouse {
     function buyBombFrom(address from, uint256 amount) external;
 }
