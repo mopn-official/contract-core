93e6ef19fbc6e875a429315ba0e1720ba1239e31
cyanface <cyanface@outlook.com>
2024-03-21 20:14:30 +0800

    debug

--- contracts/upgradeInitializers/DiamondInit.sol
+++ contracts/upgradeInitializers/DiamondInit.sol
@@ -24,16 +24,18 @@ contract DiamondInit {
     // data to set your own state variables
     function init() external {
         // adding ERC165 data
-        LibDiamond.DiamondStorage storage ds = LibDiamond.diamondStorage();
-        ds.supportedInterfaces[type(IERC165).interfaceId] = true;
-        ds.supportedInterfaces[type(IDiamondCut).interfaceId] = true;
-        ds.supportedInterfaces[type(IDiamondLoupe).interfaceId] = true;
-        ds.supportedInterfaces[type(IERC173).interfaceId] = true;
+        // LibDiamond.DiamondStorage storage ds = LibDiamond.diamondStorage();
+        // ds.supportedInterfaces[type(IERC165).interfaceId] = true;
+        // ds.supportedInterfaces[type(IDiamondCut).interfaceId] = true;
+        // ds.supportedInterfaces[type(IDiamondLoupe).interfaceId] = true;
+        // ds.supportedInterfaces[type(IERC173).interfaceId] = true;
 
         LibMOPN.MOPNStorage storage ms = LibMOPN.mopnStorage();
-        ms.LastTickTimestamp = uint32(block.timestamp);
-        ms.MTOutputPerTimestamp = 5000000;
-        ms.MTStepStartTimestamp = ms.LastTickTimestamp;
-        ms.gasrecipient = 0xC9ea36e82C647DC36012c586BF16823D598AFC36;
+        ms.PerMOPNPointMinted = 1;
+
+        // ms.LastTickTimestamp = uint32(block.timestamp);
+        // ms.MTOutputPerTimestamp = 5000000;
+        // ms.MTStepStartTimestamp = ms.LastTickTimestamp;
+        // ms.gasrecipient = 0xC9ea36e82C647DC36012c586BF16823D598AFC36;
     }
 }
